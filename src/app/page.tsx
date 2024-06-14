036ffb2098143372bb1c00154de6ae62aa2e0743
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2024-06-14 16:08:41 +1200

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-if ("geolocation" in navigator) {
-  console.log("geolocation is available");
-  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position.coords.latitude, position.coords.longitude);
-  });
-} else {
-  console.log("geolocation IS NOT available");
-}
+import { useLayoutEffect } from "react";
 
 export default function Home() {
+  useLayoutEffect(() => {
+    if ("geolocation" in navigator) {
+      console.log("geolocation is available");
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position.coords.latitude, position.coords.longitude);
+      });
+    } else {
+      console.log("geolocation IS NOT available");
+    }
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Arrive-next</h1>
+    </main>
   );
 }
